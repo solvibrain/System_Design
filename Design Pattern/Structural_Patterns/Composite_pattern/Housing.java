f22b6c39498f1f9ff3fe24dcd12cacce1b235674
solvibrain <123573736+solvibrain@users.noreply.github.com>
2024-06-12 15:35:01 +0530

    Created Interface of the Overall Structure and Created Composite Class Demonstrating Housing Example

---
+++ Design Pattern/Structural_Patterns/Composite_pattern/Housing.java
@@ -0,0 +1,12 @@
+public class Housing implements Istructure{
+    private ArrayList<Istructure> structures;
+    private String address;
+
+    public Housing(String address){
+        this.address = address;
+    }
+
+    public int addStructure(Istructure component){
+        
+    }
+}
---
+++ Design Pattern/Structural_Patterns/Composite_pattern/Istructure.java
@@ -0,0 +1,6 @@
+public interface Istructure{
+    public void enter();
+    public void exit();
+    public void location();
+    public String getName();
+}
